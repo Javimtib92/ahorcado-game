e173713b170366cc1f979c4cbe27d83ce50e8a2a
javimtib92 <javimtib92@gmail.com>
2015-01-03 13:15:39 +0100

    Styles applied and some work into game controller

--- components/ahorcado-controller.js
+++ components/ahorcado-controller.js
@@ -2,4 +2,14 @@
 angular.module('ahorcadoController',[])
     .controller('gameController', ['$scope', 'appFactory', function($scope, appFactory) {
         $scope.palabraSecreta = appFactory.getPalabraSecreta();
+        $scope.palabraSecretaArray = $scope.palabraSecreta.split("");
+        $scope.dashArray = [];
+
+        $scope.dashArrayFill = function() {
+            var i;
+            for(i = 0; i < $scope.palabraSecretaArray.length; i++) {
+                $scope.dashArray[i] = "_";
+            }
+        };
+        $scope.dashArrayFill();
     }]);
