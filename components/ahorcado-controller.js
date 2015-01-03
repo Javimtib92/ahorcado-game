81ff4214b6bcc945848a739fbc26700aedf40104
javimtib92 <javimtib92@gmail.com>
2015-01-03 13:57:11 +0100

    Update game partial

--- components/ahorcado-controller.js
+++ components/ahorcado-controller.js
@@ -3,13 +3,9 @@ angular.module('ahorcadoController',[])
     .controller('gameController', ['$scope', 'appFactory', function($scope, appFactory) {
         $scope.palabraSecreta = appFactory.getPalabraSecreta();
         $scope.palabraSecretaArray = $scope.palabraSecreta.split("");
-        $scope.dashArray = [];
 
-        $scope.dashArrayFill = function() {
-            var i;
-            for(i = 0; i < $scope.palabraSecretaArray.length; i++) {
-                $scope.dashArray[i] = "_";
-            }
-        };
-        $scope.dashArrayFill();
+        $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
     }]);
