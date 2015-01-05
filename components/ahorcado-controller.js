de726c7d350e3700dc5ce1f200003582852ecd56
javimtib92 <javimtib92@gmail.com>
2015-01-05 17:00:32 +0100

    Added some game functionality

--- components/ahorcado-controller.js
+++ components/ahorcado-controller.js
@@ -3,9 +3,19 @@ angular.module('ahorcadoController',[])
     .controller('gameController', ['$scope', 'appFactory', function($scope, appFactory) {
         $scope.palabraSecreta = appFactory.getPalabraSecreta();
         $scope.palabraSecretaArray = $scope.palabraSecreta.split("");
-
+        $scope.fillerArray = [];
+        
         $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
             'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R',
             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+        
+        $scope.submitLetter = function(letter) {
+            for(var i = 0; i < $scope.palabraSecretaArray.length; i++) {
+                if($scope.palabraSecretaArray[i] === letter.toLowerCase()) {
+                    $scope.fillerArray[i] = letter;
+                }
+            }
+        }
+        
     }]);
