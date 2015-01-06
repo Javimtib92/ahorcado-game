18e1ce7f0dbb73671753e309bc96973889422569
javimtib92 <javimtib92@gmail.com>
2015-01-06 16:44:10 +0100

    Added more game functionality

--- components/ahorcado-controller.js
+++ components/ahorcado-controller.js
@@ -4,18 +4,34 @@ angular.module('ahorcadoController',[])
         $scope.palabraSecreta = appFactory.getPalabraSecreta();
         $scope.palabraSecretaArray = $scope.palabraSecreta.split("");
         $scope.fillerArray = [];
+        $scope.errors = [];
         
         $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
             'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R',
             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-        
         $scope.submitLetter = function(letter) {
-            for(var i = 0; i < $scope.palabraSecretaArray.length; i++) {
-                if($scope.palabraSecretaArray[i] === letter.toLowerCase()) {
-                    $scope.fillerArray[i] = letter;
+            var error = null;
+            if(this.active == undefined) {
+                for(var i = 0; i < $scope.palabraSecretaArray.length; i++) {
+                    if($scope.palabraSecretaArray[i] === letter.toLowerCase()) {
+                        $scope.fillerArray[i] = letter;  
+                        error = false;
+                    }
                 }
             }
-        }
-        
+            
+            if(error != false) {
+                var finded = false;
+                for(var i = 0; i < $scope.errors.length; i++) {
+                    if($scope.errors[i] == letter) {
+                        finded = true;
+                    }
+                }
+                
+                if(!finded) {
+                    $scope.errors.push(letter);
+                } 
+            }
+        };
     }]);
